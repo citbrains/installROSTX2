cf25f5eaf2c0b100c4ed4156eab7959da2547bed
jsupratman13 <supratmanjoshua@gmail.com>
2019-01-09 14:47:59 +0900

    remove ros ip setup

--- setupCatkinWorkspace.sh
+++ setupCatkinWorkspace.sh
@@ -24,7 +24,6 @@ catkin build
 
 #setup ROS environment variables
 grep -q -F ' ROS_MASTER_URI' ~/.bashrc ||  echo 'export ROS_MASTER_URI=http://localhost:11311' | tee -a ~/.bashrc
-grep -q -F ' ROS_IP' ~/.bashrc ||  echo "export ROS_IP=$(hostname -I)" | tee -a ~/.bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
 
 
